2635106ba8d8cce269fa6b1f403842ed949424b5
Sam van de Kreeke <samkreeke3@gmail.com>
2018-04-04 13:13:08 +0200

    Begin van het project

---
+++ CeeLearnAndDo/CeeLearnAndDo/Models/MyModels/Answer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CeeLearnAndDo.Models.MyModels
+{
+    public class Answer
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public DateTime Date { get; set; }
+
+        public Question Question { get; set; }
+        public int QuestionId { get; set; }
+        
+        public ApplicationUser User { get; set; }
+        public int UserId { get; set; } 
+    }
+}
---
+++ CeeLearnAndDo/CeeLearnAndDo/Models/MyModels/Article.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CeeLearnAndDo.Models.MyModels
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Photo { get; set; }
+        public DateTime Date { get; set; }
+        public bool Published { get; set; }
+
+        public ApplicationUser User { get; set; }
+        public int UserId { get; set; }
+    }
+}
